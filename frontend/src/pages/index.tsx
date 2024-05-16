a2266190713563002e1183119bd12353ab63e0b7
6sous <cyrille.aquilina@gmail.com>
2024-05-16 15:41:39 +0200

    need to push

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import { Continent, CountryType } from "@/types";
 import { gql, useLazyQuery, useMutation, useQuery } from "@apollo/client";
 import Link from "next/link";
@@ -70,74 +71,75 @@ export default function Home() {
   if (error) return <h1>Error</h1>;
 
   return (
-    <>
-      <h1>Countries</h1>
+    <div className="countries-page">
+      <Header />
+      <main>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
 
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
+            const formData = new FormData(event.target as HTMLFormElement);
 
-          const formData = new FormData(event.target as HTMLFormElement);
+            const entries = Object.fromEntries(formData.entries());
 
-          const entries = Object.fromEntries(formData.entries());
+            const newCountry = {
+              code: entries.code,
+              name: entries.name,
+              emoji: entries.emoji,
+              continent: {
+                id: Number(entries.continent),
+              },
+            };
 
-          const newCountry = {
-            code: entries.code,
-            name: entries.name,
-            emoji: entries.emoji,
-            continent: {
-              id: Number(entries.continent),
-            },
-          };
+            console.log(newCountry);
 
-          console.log(newCountry);
+            addCountry({
+              variables: { data: newCountry },
+              onCompleted: () => {
+                fetchCountries();
+              },
+            });
+          }}>
+          <label htmlFor="name">
+            Name
+            <input type="text" name="name" id="name" />
+          </label>
+          <label htmlFor="code">
+            Code
+            <input type="text" name="code" id="code" />
+          </label>
+          <label htmlFor="emoji">
+            Emoji
+            <input type="text" name="emoji" id="emoji" />
+          </label>
+          <label htmlFor="continent">
+            Continent
+            <select name="continent" id="continent">
+              {continents &&
+                continents.map((continent: Continent) => (
+                  <option key={continent.id} value={continent.id}>
+                    {continent.name}
+                  </option>
+                ))}
+            </select>
+          </label>
+          <button type="submit">Add new country</button>
+        </form>
 
-          addCountry({
-            variables: { data: newCountry },
-            onCompleted: () => {
-              fetchCountries();
-            },
-          });
-        }}>
-        <label htmlFor="name">
-          Name
-          <input type="text" name="name" id="name" />
-        </label>
-        <label htmlFor="code">
-          Code
-          <input type="text" name="code" id="code" />
-        </label>
-        <label htmlFor="emoji">
-          Emoji
-          <input type="text" name="emoji" id="emoji" />
-        </label>
-        <label htmlFor="continent">
-          Continent
-          <select name="continent" id="continent">
-            {continents &&
-              continents.map((continent: Continent) => (
-                <option key={continent.id} value={continent.id}>
-                  {continent.name}
-                </option>
-              ))}
-          </select>
-        </label>
-        <button type="submit">Add new country</button>
-      </form>
-
-      <section className="countries">
-        {countries &&
-          countries.map((country: CountryType) => (
-            <Link
-              href={`/country/${country.code}`}
-              className="country"
-              key={country.id}>
-              <h3>{country.name}</h3>
-              <span>{country.emoji}</span>
-            </Link>
-          ))}
-      </section>
-    </>
+        <section className="countries">
+          {countries &&
+            countries.map((country: CountryType) => (
+              <Link
+                href={`/country/${country.code}`}
+                className="country"
+                key={country.id}>
+                <h3>{country.name}</h3>
+                <span>{country.emoji}</span>
+              </Link>
+            ))}
+        </section>
+      </main>
+    </div>
   );
 }
 
